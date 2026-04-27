8364dd554e71a5f9ad457f02e686ce3b9383a024
josiahsimorangkir <josiahsimorangkir@gmail.com>
2026-04-28 00:41:41 +0700

    [Refactoring] - Optimizing /highest-gpa endpoints

--- src/main/java/com/advpro/profiling/tutorial/service/StudentService.java
+++ src/main/java/com/advpro/profiling/tutorial/service/StudentService.java
@@ -6,6 +6,7 @@ import com.advpro.profiling.tutorial.repository.StudentCourseRepository;
 import com.advpro.profiling.tutorial.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.data.domain.PageRequest;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -28,16 +29,8 @@ public class StudentService {
     }
 
     public Optional<Student> findStudentWithHighestGpa() {
-        List<Student> students = studentRepository.findAll();
-        Student highestGpaStudent = null;
-        double highestGpa = 0.0;
-        for (Student student : students) {
-            if (student.getGpa() > highestGpa) {
-                highestGpa = student.getGpa();
-                highestGpaStudent = student;
-            }
-        }
-        return Optional.ofNullable(highestGpaStudent);
+        List<Student> result = studentRepository.findHighestGpaStudent(PageRequest.of(0, 1));
+        return result.isEmpty() ? Optional.empty() : Optional.ofNullable(result.get(0));
     }
 
     public String joinStudentNames() {
